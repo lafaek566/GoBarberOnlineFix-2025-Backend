3e62569262e3e2abdec5024bbcda9fe4cee96e61
elvren <michaelenahak@gmail.com>
2025-07-07 17:38:41 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "root1234",
-  database: process.env.DB_NAME || "bGo",
-  waitForConnections: true,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   connectionLimit: 10,
   queueLimit: 0,
 });
--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use((err, req, res, next) => {
 });
 
 // Server configuration
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
